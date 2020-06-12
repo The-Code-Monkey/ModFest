af735a19427cbd80e74ed6ce5368de5c2dc21bcb
AwwShoot <parker_luce@yahoo.com>
2020-06-12 11:53:32 -0400

    added biomes folder and very basic info for the first biome

---
+++ src/main/java/net/astrum/common/biomes/AstrumBiome.java
@@ -0,0 +1,11 @@
+package net.astrum.common.biomes;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+
+
+public class AstrumBiome extends Biome {
+    public AstrumBiome(){
+        super(new Biome.Settings().configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_CONFIG).precipitation(Biome.Precipitation.RAIN).category(Biome.Category.NONE).depth(0.24F).scale(0.2F).temperature(0.6F).downfall(0.7F).parent((String)null));
+    }
+}
