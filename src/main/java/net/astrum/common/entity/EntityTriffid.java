bba65cd782dde3b6ebff6914b5d402bbb3a0e948
The-Code-Monkey <andy.wilson1234@gmail.com>
2020-06-16 16:45:38 +0100

    fix: fix issue with guardian

--- src/main/java/net/astrum/common/entity/EntityTriffid.java
+++ src/main/java/net/astrum/common/entity/EntityTriffid.java
@@ -18,6 +18,7 @@ import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
 import java.util.EnumSet;
+import java.util.Objects;
 
 public class EntityTriffid extends HostileEntity {
     private static final double HEALTH = 10.0D;
@@ -29,6 +30,7 @@ public class EntityTriffid extends HostileEntity {
     private LivingEntity cachedLineTarget;
 
     protected WanderAroundGoal wanderGoal;
+    private int lineTicks;
 
     private static final TrackedData<Integer> LINE_TARGET_ID;
 
@@ -49,7 +51,8 @@ public class EntityTriffid extends HostileEntity {
     protected void initGoals() {
         this.wanderGoal = new WanderAroundGoal(this, MAX_SPEED);
         this.targetSelector.add(1, new FollowTargetGoal<>(this, PlayerEntity.class, true));
-        this.goalSelector.add(2, new EntityTriffid.DragGoal(this));
+        this.goalSelector.add(2, new MeleeAttackGoal(this, MAX_SPEED, true));
+//        this.targetSelector.add(2, );
         this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(5, this.wanderGoal);
         this.goalSelector.add(8, new LookAroundGoal(this));
@@ -103,64 +106,12 @@ public class EntityTriffid extends HostileEntity {
         }
     }
 
-    static class DragGoal extends Goal {
-        private final EntityTriffid triffid;
-        private int lineTicks;
-
-        public DragGoal(EntityTriffid triffid) {
-            this.triffid = triffid;
-            this.setControls(EnumSet.of(Control.MOVE, Control.LOOK));
+    public void onTrackedDataSet(TrackedData<?> data) {
+        super.onTrackedDataSet(data);
+        if (LINE_TARGET_ID.equals(data)) {
+            this.lineTicks = 0;
+            this.cachedLineTarget = null;
         }
 
-        public boolean canStart() {
-            LivingEntity entity = this.triffid.getTarget();
-            return entity != null && entity.isAlive();
-        }
-
-        public boolean shouldContinue() {
-            return super.shouldContinue() && this.triffid.squaredDistanceTo(this.triffid.getTarget()) > 9.0D;
-        }
-
-        public void start() {
-            this.lineTicks = -10;
-            this.triffid.getNavigation().stop();
-            this.triffid.getLookControl().lookAt(this.triffid.getTarget(), 90.0F, 90.0F);
-            this.triffid.velocityDirty = true;
-        }
-
-        public void stop() {
-            this.triffid.setLineTarget(0);
-            this.triffid.setTarget(null);
-            this.triffid.wanderGoal.ignoreChanceOnce();
-        }
-
-        public void tick() {
-            LivingEntity livingEntity = this.triffid.getTarget();
-            System.out.println("HERE");
-            System.out.println(livingEntity);
-            this.triffid.getNavigation().stop();
-            this.triffid.getLookControl().lookAt(livingEntity, 90.0F, 90.0F);
-            if (!this.triffid.canSee(livingEntity)) {
-                this.triffid.setTarget(null);
-            } else {
-                ++lineTicks;
-                if (this.lineTicks == 0) {
-                    this.triffid.setLineTarget(this.triffid.getTarget().getEntityId());
-                    if (!this.triffid.isSilent()) {
-                        this.triffid.world.sendEntityStatus(this.triffid, (byte)21);
-                    }
-                } else if (this.lineTicks >= this.triffid.getWarmupTime()) {
-                    float f = 0.5F;
-                    if (this.triffid.world.getDifficulty() == Difficulty.HARD) {
-                        f += 1.0F;
-                    }
-
-                    Vec3d playerToMove = new Vec3d(1, 1, 1);
-
-                    assert livingEntity != null;
-                    livingEntity.move(MovementType.PLAYER, playerToMove);
-                }
-            }
-        }
     }
 }
